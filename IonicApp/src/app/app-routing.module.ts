964d0f908ffce6bba3bc1c683125b16630ea1478
Karbin92 <fredrik.karlsson1992@live.com>
2019-11-17 14:10:08 +0100

    latest changes

--- IonicApp/src/app/app-routing.module.ts
+++ IonicApp/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'series', pathMatch: 'full' },
+  
   {
     path: 'series',
     loadChildren: () => import('./pages/series/series.module').then( m => m.SeriesPageModule)
   },
   {
-    path: 'series-details',
+    path: 'series/:id',
     loadChildren: () => import('./pages/series-details/series-details.module').then( m => m.SeriesDetailsPageModule)
   },
 ];
