b0d43c1eb0c9956511e0b3a2ee5d66c263be6209
Karbin92 <fredrik.karlsson1992@live.com>
2019-11-18 12:05:49 +0100

    changes to search and UI

--- IonicApp/src/app/pages/series-details/series-details.page.ts
+++ IonicApp/src/app/pages/series-details/series-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SeriesService } from 'src/app/services/series.service';
 
 @Component({
   selector: 'app-series-details',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./series-details.page.scss'],
 })
 export class SeriesDetailsPage implements OnInit {
+  information = null; //hides the view until it is loaded
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private seriesService: SeriesService) { } //injecting activated route
 
-  ngOnInit() {
+  ngOnInit() { //is called in the beginning when the component is created, here the activated route snapshot is implemented
+    let id = this.activatedRoute.snapshot.paramMap.get('id'); //goes back to the service and grabs the data
+
+    this.seriesService.getDetails(id).subscribe(result => { //get details with the corresponding ID
+      console.log('details: ', result); //display result
+      this.information = result; //allows access to result inside the view
+    });
+  }
+
+  openWebsite() { //ensures information is loaded in the app and not in the browser
+    window.open(this.information.Website, '_blank');
   }
 
 }
